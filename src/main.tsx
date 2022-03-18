65b2daa1a533afa2eb9e170551c4ce9a18ebd6e1
knownrock <knownrock@hotmail.com>
2022-03-18 13:45:12 +0800

    add i18n

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import Theme from './themes/Main'
+import './i18n/index'
 
 ReactDOM.render(
   <React.StrictMode>
