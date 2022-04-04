910e29773d6a331d83e4333e1a62f7d7d9e21fb6
knownrock <knownrock@hotmail.com>
2022-04-04 21:29:06 +0800

    add back button

--- src/components/TopBar/index.tsx
+++ src/components/TopBar/index.tsx
@@ -1,4 +1,4 @@
-import { Container } from '@mui/material'
+import { Button, Container, IconButton } from '@mui/material'
 import AppBar from '@mui/material/AppBar'
 import Box from '@mui/material/Box'
 
@@ -6,7 +6,33 @@ import Toolbar from '@mui/material/Toolbar'
 
 import Typography from '@mui/material/Typography'
 
-function ResponsiveAppBar() {
+import { useTranslation } from 'react-i18next'
+import BackIcon from '@mui/icons-material/ArrowBack'
+import { useNavigate } from 'react-router-dom'
+
+type Props = {
+  title?: string,
+  withBack?: boolean,
+  children?: React.ReactNode,
+}
+
+const defaultProps = {
+  title: undefined,
+  withBack: false,
+  children: undefined,
+}
+
+function ResponsiveAppBar({ title, withBack, children }: Props) {
+  const { t } = useTranslation()
+  const navigate = useNavigate()
+
+  // https://stackoverflow.com/questions/66039933/typescript-types-for-import-meta-env
+  const innerTitle = title ?? t(import.meta.env.VITE_APP_TITLE)
+
+  const handleBack = () => {
+    navigate(-1)
+  }
+
   return (
     <AppBar
       position="sticky"
@@ -14,14 +40,27 @@ function ResponsiveAppBar() {
       }}
     >
       <Toolbar variant="dense">
-        {/* <Container maxWidth="lg"> */}
-
+        {withBack && (
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+            onClick={handleBack}
+          >
+            <BackIcon />
+          </IconButton>
+        )}
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          {import.meta.env.VITE_APP_TITLE}
+          {innerTitle}
         </Typography>
-        {/* </Container> */}
+        {children}
+
       </Toolbar>
     </AppBar>
   )
 }
+
+ResponsiveAppBar.defaultProps = defaultProps
 export default ResponsiveAppBar
